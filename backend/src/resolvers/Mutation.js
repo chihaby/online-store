5bfe167a9e9dde138b97a7373dab23f5374c03c9
Rad <chihaby@gmail.com>
2020-01-25 10:59:53 -0800

    error in prisma.graphql

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,4 +1,12 @@
 const Mutations = {
+  async createItem(parent, args, ctx, info){
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args,
+      }
+    }, info);
+    return item;
+  }
   // createDog(parent, args, ctx, info){
   //   // create a dog!
   //   const newDog = {name: args.name };
