3b4993d3fc0240fe2d99cb85035c31941f000b06
Rad <chihaby@gmail.com>
2020-02-05 19:52:50 -0800

    adds SingleItem component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "145b4859bf52f7141536";
+/******/ 	var hotCurrentHash = "fae363208d7d3b57dee1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
