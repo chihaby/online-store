5f3b1a838a2052a127e81fbedcaa7b129b24d1b9
Rad <chihaby@gmail.com>
2020-02-07 11:15:51 -0800

    password reset on backend

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48546bf509683affc008";
+/******/ 	var hotCurrentHash = "b02518ce9ba08adc7fa3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
