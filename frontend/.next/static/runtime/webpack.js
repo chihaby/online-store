64e9cf62cfcc97a4753b881622dcac99a80d7a40
Rad <chihaby@gmail.com>
2020-03-05 09:37:38 -0800

    adds items to the cart

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1539ca74484bce55c677";
+/******/ 	var hotCurrentHash = "ba03cb5137a2eac6a60a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
