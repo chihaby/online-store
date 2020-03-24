0f0c5ee8b56ba901bfd7e2204f13af23f730b1e3
Rad <chihaby@gmail.com>
2020-03-23 20:02:32 -0700

    creadit cartd process

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a03a6c3700e1e448f582";
+/******/ 	var hotCurrentHash = "875d2b32305f8b5a1487";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
